6b01aba6659abe5c8b37569b2719d74fe5cfaec6
plaguedoc18 <pivovarov_96@mail.ru>
2018-04-16 12:57:17 +0400

    add test

---
+++ ItUniver.TeleCalc.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ItUniver.TeleCalc.ConCalc;
+
+namespace ItUniver.TeleCalc.Tests
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+
+            //Arrange
+            var calc = new Calc();
+            var x = 1;
+            var y = 2;
+            
+            //Act
+            var result1 = calc.Sum(x, y);
+            var result2 = calc.Sum(10, 0);
+
+            //Assert
+            Assert.AreEqual(3, result1);
+            Assert.AreEqual(10, result2);
+
+        }
+    }
+}
